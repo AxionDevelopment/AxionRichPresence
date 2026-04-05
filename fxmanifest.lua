232b54f4ed0597c8de9174e23787de1d26e51b9b
SpunkyDunkie <48916878+spunkydunkie@users.noreply.github.com>
2026-04-04 20:50:49 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AxionDevelopment | SpunkyDunkie'
+description 'Axion Rich Presence'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_script 'configs/richpresence.config.lua'
+
+client_scripts {
+    'scripts/client.lua'
+}
+
+server_scripts {
+    'scripts/server.lua'
+}
---
+++ scripts/server.lua
@@ -0,0 +1,15 @@
+AddEventHandler('playerConnecting', function()
+    local src = source
+    local maxPlayers = GetConvarInt("sv_maxClients", 32)
+    print("SERVER: receiveMaxPlayers triggered")
+    TriggerClientEvent("richpresence:receiveMaxPlayers", src, maxPlayers)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local playerCount = #GetPlayers()
+        print("SERVER: returning player count")
+        TriggerClientEvent('richpresence:returnPlayerCount', -1, playerCount)
+        Citizen.Wait(10000)
+    end
+end)
